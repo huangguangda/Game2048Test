59bb986dc68a18e30c0bafb3fbacb0dd8a88caf6
huangguangda <2397923107@qq.com>
2018-01-14 02:36:30 +0800

    编写程序

--- app/src/main/java/cn/edu/gdmec/android/game2048test/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/game2048test/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.edu.gdmec.android.game2048test;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvMaxScore;
     private Button btRestart;
     private GameView gameView;
+
+    public static MainActivity mainActivity;
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private int score, maxscore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
