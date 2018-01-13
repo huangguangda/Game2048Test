483a1d920641820000cbd06f8eff398681650a2b
huangguangda <2397923107@qq.com>
2018-01-14 02:32:59 +0800

    添加权限

---
+++ app/src/main/java/cn/edu/gdmec/android/game2048test/GameView.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.game2048test;
+
+/**
+ * Created by Jack on 2018/1/14.
+ */
+
+public class GameView {
+}
--- app/src/main/java/cn/edu/gdmec/android/game2048test/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/game2048test/MainActivity.java
@@ -2,8 +2,15 @@ package cn.edu.gdmec.android.game2048test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvScore;
+    private TextView tvMaxScore;
+    private Button btRestart;
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
